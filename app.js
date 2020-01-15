d1b2224206308d6ef1fe6f782781101dd5d381fa
Tomxss <46247615+Tomxss@users.noreply.github.com>
2020-01-15 15:57:44 +0200

    Completed Default Template

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express'); //Allows me to run a server
-const chalk = require('chalk'); //Debugging my code with color
+const express = require('express'); // Allows me to run a server
+const chalk = require('chalk'); // Debugging my code with color
+const debug = require('debug')('app'); // Removes the need to use console.log, but only runs when in Debug mode.
+const morgan = require('morgan'); // Logs web traffic, morgan is known as middleware
 
 const app = express();
 var port = 4000;
+
+app.use(morgan('tiny'));// combined: more verbose
 
 app.get('/', (req, res) => {
   res.send("Hello world");
 });
 
 app.listen(port, (req, res) => {
-  console.log(`Listening on port: ${chalk.green(port)}`);
+  debug(`Listening on port: ${chalk.green(port)}`);
 });
