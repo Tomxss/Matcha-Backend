eb5709036317f3a670bd4217b3d843f8ec5c5e44
Tomxss <46247615+Tomxss@users.noreply.github.com>
2020-01-16 11:44:22 +0200

    struggling with routing back to previous route...

--- app.js
+++ app.js
@@ -5,16 +5,36 @@ const morgan = require('morgan'); // Logs web traffic, morgan is known as middle
 const path = require('path');
 
 const app = express();
-var port = 4000;
+const port = process.env.PORT || 3000;
+const navRouter = express.Router();
 
 app.use(morgan('tiny')); // combined: more verbose
 app.use(express.static(path.join(__dirname, 'public'))); // letting express know where my static files are: css/js
 app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css'))); // will recognise this as another css file
 app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+navRouter.route('/register')
+  .get((req, res) => {
+    res.render('register');
+  });
+
+navRouter.route('/login')
+.get((req, res) => {
+  res.render('login');
+});
+
+app.use('/', navRouter);
+app.use('/register', navRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));  //__dirname: location of current app
+  res.render('index', { 
+    topBar: [{title:'Register', link: '/register'}, 
+            {title:'Login', link:'/login'}], 
+    title: 'Matcha' 
+  });  //__dirname: location of current app
 });
 
 app.listen(port, (req, res) => {
